e1a8c1003fd70d304a962a8c7b7135f545e1a8cc
Bartosz Borys <borysbartosz1@gmail.com>
2020-07-23 16:36:15 +0200

    refactor - change names, change function order etc, performance - cache background class to prevent angular recalculation, unsubscribe subject after destroy

--- frontend/src/app/modules/shared/modules/notifications/notifications.component.ts
+++ frontend/src/app/modules/shared/modules/notifications/notifications.component.ts
@@ -1,67 +1,72 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { NotificationsService } from './services/notifications/notifications.service';
 import { Notifications } from './interfaces/notifications.interface';
 import { NotificationsTypes } from './constants/notifications-types.constant';
 import { NotificationProgressBarComponent } from './components/notification-progress-bar/notification-progress-bar.component';
+import { not } from '@angular/compiler/src/output/output_ast';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.scss'],
 })
-export class NotificationsComponent implements AfterViewInit {
+export class NotificationsComponent implements AfterViewInit, OnDestroy {
   @ViewChild(NotificationProgressBarComponent)
   public readonly progressBar?: NotificationProgressBarComponent;
 
   public readonly messages: Notifications[] = [];
   public readonly hideTimeout: number = 2500;
-  public isHidden: boolean = true;
+
+  public notificationsBus: Subscription;
+  public isContentHidden: boolean = true;
   public selected?: Notifications = null;
+  public backgroundClass: string = "";  
 
   private currentTimeout?: number | any;
 
   constructor(private readonly notifications: NotificationsService) { }
 
   public ngAfterViewInit(): void {
-    this.notifications.bus.subscribe({
-      next: (notification: Notifications) => {
-        this.messages.push(notification);
-        this.select(notification);
-        this.showMessage();
-        this.setHideTimeout();
-      },
+    this.notificationsBus = this.notifications.bus.subscribe({
+      next: (notification: Notifications) => this.newNotification(notification),
     });
   }
 
-  public remove(): void {
-    this.messages.splice(this.messages.indexOf(this.selected), 1);
-    this.selected = this.messages.length > 0 ? this.messages[0] : null;
+  public ngOnDestroy(): void {
+    this.notificationsBus.unsubscribe();
   }
 
-  public select(notification: Notifications): void {
-    if(this.selected === notification) {
-      this.isHidden = !this.isHidden;
-    } else {
-      this.showMessage();
-      this.selected = notification;
-    }
+  private newNotification(notification: Notifications): void {
+    this.messages.push(notification);
+    this.select(notification);
+    this.showContent();
+    this.runHideCountdown();
   }
 
-  public setHideTimeout() {
-    if(this.isHidden) {
+  public get isNotificationsEmpty() {
+    return this.messages?.length <= 0;
+  }
+
+  public isSelected(message: Notifications) {
+    return !this.isContentHidden && (this.selected === message);
+  }
+
+  public runHideCountdown() {
+    if(this.isContentHidden) {
       return;
     }
 
-    this.clearHideTimeout();
+    this.clearHideCountdown();
 
     this.progressBar?.run();
     this.currentTimeout = setTimeout(()=> {
-      this.hideMessage();
+      this.hideContent();
     }, this.hideTimeout);
   }
 
-  public clearHideTimeout() {
-    if(this.isHidden) {
+  public clearHideCountdown() {
+    if(this.isContentHidden) {
       return;
     }
 
@@ -72,20 +77,62 @@ export class NotificationsComponent implements AfterViewInit {
     }
   }
 
-  private showMessage(): void {
-    this.isHidden = false;
+  private showContent(): void {
+    this.isContentHidden = false;
   }
 
-  private hideMessage(): void {
-    this.isHidden = true;
+  private toggleContent(): void {
+    this.isContentHidden = !this.isContentHidden;
   }
 
-  public get backgroundClass(): Object {
-    return {
-      'is-success': this.selected?.type === NotificationsTypes.Success,
-      'is-warning': this.selected?.type === NotificationsTypes.Warning,
-      'is-danger': this.selected?.type === NotificationsTypes.Error,
-      'is-link': this.selected?.type === NotificationsTypes.Info,
-    };
+  private hideContent(): void {
+    this.isContentHidden = true;
+  }
+
+  public remove(): void {
+    this.messages.splice(this.messages.indexOf(this.selected), 1);
+    this.selected = this.messages.length > 0 ? this.messages[0] : null;
+  }
+
+  public select(notification: Notifications): void {
+    if(this.selected === notification) {
+      this.toggleContent();
+    } else {
+      this.changeContentTo(notification);
+    }
+    
+    this.setBackgroundClass(notification.type);
+  }
+
+  private changeContentTo(newSelected: Notifications) {
+    this.selected = newSelected;
+    this.showContent();
+  }
+
+  private setBackgroundClass(type: NotificationsTypes) {
+    const backgroundClass = this.getBackgroundColorClassFor(type);
+    if(this.backgroundClass === backgroundClass) {
+      return;
+    }
+    this.backgroundClass = backgroundClass;
+  }
+
+  private getBackgroundColorClassFor(type: NotificationsTypes): string {
+    switch(type) {
+      case NotificationsTypes.Success:
+        return 'is-success';
+
+      case NotificationsTypes.Warning:
+        return 'is-warning';
+
+      case NotificationsTypes.Error:
+        return 'is-danger';
+
+      case NotificationsTypes.Info:
+        return 'is-link';
+      
+      default:
+        throw new Error("Unknown notification type");
+    }
   }
 }
