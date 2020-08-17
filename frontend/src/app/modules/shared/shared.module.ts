32ee1b309bcb4b171b07cbcc5d7054dc1e111cd2
Bartosz Borys <borysbartosz1@gmail.com>
2020-08-17 21:28:48 +0200

    add change-language component to shared module

--- frontend/src/app/modules/shared/shared.module.ts
+++ frontend/src/app/modules/shared/shared.module.ts
@@ -3,17 +3,22 @@ import { CommonModule } from '@angular/common';
 import { NotificationsModule } from './modules/notifications/notifications.module';
 import { FormElementsModule } from './modules/form-elements/form-elements.module';
 import { TranslateModule } from '@ngx-translate/core';
-import { ChildTranslationModule } from 'src/app/translate/translate';
+import { ChildTranslationModule } from './constants/translate.constant';
+import { FormsModule } from '@angular/forms';
+import { ChangeLanguageComponent } from './components/change-language/change-language.component';
 
 @NgModule({
   declarations: [
+    ChangeLanguageComponent,
   ],
   exports: [
+    ChangeLanguageComponent,
     NotificationsModule,
     FormElementsModule,
     TranslateModule,
   ],
   imports: [
+    FormsModule,
     FormElementsModule,
     NotificationsModule,
     CommonModule,
